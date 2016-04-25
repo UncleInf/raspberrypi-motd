9bd6c25d6bc1e0d61947bb0473ec0b1c61754f50
Detmud <detmud@me.com>
2016-04-25 22:22:32 +0200

    Update motd.sh

--- motd.sh
+++ motd.sh
@@ -1,167 +1,45 @@
 #!/bin/bash
-
 clear
 
-function color (){
-  echo "\e[$1m$2\e[0m"
-}
+VAR_UPTIME="$(uptime | sed -E 's/^[^,]*up *//; s/, *[[:digit:]]* user.*//; s/min/minutes/; s/([[:digit:]]+):0?([[:digit:]]+)/\1 hours, \2 minu
+tes/')"
+VAR_MEMORY="$(free -m | awk 'NR==2 { printf "Total: %sMB, Used: %sMB, Free: %sMB",$2,$3,$4; }')"
+VAR_SPACE="$(df -h ~ | awk 'NR==2 { printf "Total: %sB, Used: %sB, Free: %sB",$2,$3,$4; }')"
+VAR_LOADAVG="$(uptime |  awk '{print $8,$9,$10}')"
+VAR_PROCESSES="$(ps ax | wc -l | tr -d " ")"
+VAR_IP_INTERN="$(hostname -I)"
+VAR_IP_EXTERN="$(wget -q -O - http://icanhazip.com/ | tail)"
+VAR_TEMP="$(/opt/vc/bin/vcgencmd measure_temp | cut -c "6-9")°C"
+VAR_LAST_LOGIN="$(last -i $USER -F | grep -v 'still logged' | head -1 | awk '{print $6,$5,$8,$7}')"
+VAR_DATE="$(date +"%A, %e %B %Y, %R")"
+VAR_UNAME="$(uname -snrmo)"
 
-function extend (){
-  local str="$1"
-  let spaces=60-${#1}
-  while [ $spaces -gt 0 ]; do
-    str="$str "
-    let spaces=spaces-1
-  done
-  echo "$str"
-}
+SERVICE_1='airsonos'
 
-function center (){
-  local str="$1"
-  let spacesLeft=(78-${#1})/2
-  let spacesRight=78-spacesLeft-${#1}
-  while [ $spacesLeft -gt 0 ]; do
-    str=" $str"
-    let spacesLeft=spacesLeft-1
-  done
-
-  while [ $spacesRight -gt 0 ]; do
-    str="$str "
-    let spacesRight=spacesRight-1
-  done
-
-  echo "$str"
-}
-
-function sec2time (){
-  local input=$1
-
-  if [ $input -lt 60 ]; then
-    echo "$input seconds"
-  else
-    ((days=input/86400))
-    ((input=input%86400))
-    ((hours=input/3600))
-    ((input=input%3600))
-    ((mins=input/60))
-
-    local daysPlural="s"
-    local hoursPlural="s"
-    local minsPlural="s"
-
-    if [ $days -eq 1 ]; then
-      daysPlural=""
-    fi
-
-    if [ $hours -eq 1 ]; then
-      hoursPlural=""
-    fi
-
-    if [ $mins -eq 1 ]; then
-      minsPlural=""
-    fi
-
-    echo "$days day$daysPlural, $hours hour$hoursPlural, $mins minute$minsPlural"
-  fi
-}
-
-
-function check-ifstatus() {
-FOUND=`grep "eth0:\|wlan0:\|wlan1:\|usb0" /proc/net/dev`
-    if  [ -n "$FOUND" ] ; then
-      label$FOUND=""
-    fi
-}
-
-
-
-borderColor=35
-headerLeafColor=32
-headerRaspberryColor=31
-greetingsColor=36
-statsLabelColor=33
-
-borderLine="━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-borderTopLine=$(color $borderColor "┏$borderLine┓")
-borderBottomLine=$(color $borderColor "┗$borderLine┛")
-borderBar=$(color $borderColor "┃")
-borderEmptyLine="$borderBar                                                                              $borderBar"
-
-# Header
-header="$borderTopLine\n$borderEmptyLine\n"
-header="$header$borderBar$(color $headerLeafColor "          .~~.   .~~.                                                         ")$borderBar\n"
-header="$header$borderBar$(color $headerLeafColor "         '. \ ' ' / .'                                                        ")$borderBar\n"
-header="$header$borderBar$(color $headerRaspberryColor "          .~ .~~~..~.                      _                          _       ")$borderBar\n"
-header="$header$borderBar$(color $headerRaspberryColor "         : .~.'~'.~. :     ___ ___ ___ ___| |_ ___ ___ ___ _ _    ___|_|      ")$borderBar\n"
-header="$header$borderBar$(color $headerRaspberryColor "        ~ (   ) (   ) ~   |  _| .'|_ -| . | . | -_|  _|  _| | |  | . | |      ")$borderBar\n"
-header="$header$borderBar$(color $headerRaspberryColor "       ( : '~'.~.'~' : )  |_| |__,|___|  _|___|___|_| |_| |_  |  |  _|_|      ")$borderBar\n"
-header="$header$borderBar$(color $headerRaspberryColor "        ~ .~ (   ) ~. ~               |_|                 |___|  |_|          ")$borderBar\n"
-header="$header$borderBar$(color $headerRaspberryColor "         (  : '~' :  )                                                        ")$borderBar\n"
-header="$header$borderBar$(color $headerRaspberryColor "          '~ .~~~. ~'                                                         ")$borderBar\n"
-header="$header$borderBar$(color $headerRaspberryColor "              '~'                                                             ")$borderBar"
-
-me=$(whoami)
-
-# Greetings
-greetings="$borderBar$(color $greetingsColor "$(center "Welcome back, $me!")")$borderBar\n"
-greetings="$greetings$borderBar$(color $greetingsColor "$(center "$(date +"%A, %d %B %Y, %T")")")$borderBar"
-
-# System information
-read loginFrom loginIP loginDate loginTime <<< $(last $me | awk 'NR==2 { print $2,$3,$4,$7 }')
-
-# TTY login
-if [[ $loginDate == - ]]; then
-  loginDate=$loginIP
-  loginIP=$loginFrom
-fi
-
-if [[ $loginDate == *T* ]]; then
-  login="$(date -d $loginDate +"%A, %d %B %Y,") $loginTime ($loginIP)"
+if ps ax | grep -v grep | grep $SERVICE_1 > /dev/null
+then
+	LABEL_SERVICE_1="running, everything is fine"
 else
-  # Not enough logins
-  login="None"
+	LABEL_SERVICE_1="not running"
 fi
 
-label1="$(extend "$login")"
-label1="$borderBar  $(color $statsLabelColor "Last Login....:") $label1$borderBar"
+RED=$(tput setaf 1)
+GRE=$(tput setaf 2)
+YEL=$(tput setaf 3)
+BLU=$(tput setaf 4)
+WHI=$(tput setaf 7)
 
-uptime="$(sec2time $(cut -d "." -f 1 /proc/uptime))"
-uptime="$uptime ($(date -d "@"$(grep btime /proc/stat | cut -d " " -f 2) +"%d-%m-%Y %H:%M:%S"))"
-
-label2="$(extend "$uptime")"
-label2="$borderBar  $(color $statsLabelColor "Uptime........:") $label2$borderBar"
-
-label3="$(extend "$(free -m | awk 'NR==2 { printf "Total: %sMB, Used: %sMB, Free: %sMB",$2,$3,$4; }')")"
-label3="$borderBar  $(color $statsLabelColor "Memory........:") $label3$borderBar"
-
-label4="$(extend "$(df -h ~ | awk 'NR==2 { printf "Total: %sB, Used: %sB, Free: %sB",$2,$3,$4; }')")"
-label4="$borderBar  $(color $statsLabelColor "Home space....:") $label4$borderBar"
-
-label5="$(extend "$(/opt/vc/bin/vcgencmd measure_temp | cut -c "6-9")ºC")"
-label5="$borderBar  $(color $statsLabelColor "Temperature...:") $label5$borderBar"
-
-labeleth0="$(extend "$(ifconfig eth0 | grep "inet ad" | cut -f2 -d: | awk '{print $1}')")"
-labeleth0="$borderBar  $(color $statsLabelColor "IP of eth0....:") $labeleth0$borderBar"
-
-labelwlan0="$(extend "$(ifconfig wlan0 | grep "inet ad" | cut -f2 -d: | awk '{print $1}')")"
-labelwlan0="$borderBar  $(color $statsLabelColor "IP of wlan0...:") $labelwlan0$borderBar"
-
-labelwlan1="$(extend "$(ifconfig wlan1 | grep "inet ad" | cut -f2 -d: | awk '{print $1}')")"
-labelwlan1="$borderBar  $(color $statsLabelColor "IP of wlan1...:") $labelwlan1$borderBar"
-
-labelusb0="$(extend "$(ifconfig usb0 | grep "inet ad" | cut -f2 -d: | awk '{print $1}')")"
-labelusb0="$borderBar  $(color $statsLabelColor "IP of usb0....:") $labelusb0$borderBar"
-
-labelIPv4="$(extend "$(wget -q -O - http://ipv4.icanhazip.com/ | tail)")"
-labelIPv4="$borderBar  $(color $statsLabelColor "WAN IPv4......:") $labelIPv4$borderBar"
-
-labelIPv6="$(extend "$(wget -q -O - http://ipv6.icanhazip.com/ | tail)")"
-labelIPv6="$borderBar  $(color $statsLabelColor "WAN IPv6......:") $labelIPv6$borderBar"
-
-stats="$label1\n$label2\n$label3\n$label4\n$label5\n$labeleth0\n$labelwlan0\n$labelwlan1\n$labelusb0\n$labelIPv4\n$labelIPv6"
-
-
-# Print motd
-
-#check-ifstatus
-echo -e "$header\n$borderEmptyLine\n$greetings\n$borderEmptyLine\n$stats\n$borderEmptyLine\n$borderBottomLine"
+echo "${BLU}${VAR_DATE}"
+echo "${BLU}${VAR_UNAME}"
+echo ""
+echo "${GRE}   .~~.   .~~.     ${YEL}Last Login.........:${WHI} ${VAR_LAST_LOGIN}"
+echo "${GRE}  '. \ ' ' / .'    ${YEL}Uptime ............:${WHI} ${VAR_UPTIME}"
+echo "${RED}   .~ .~~~..~.     ${YEL}Load Average.......:${WHI} ${VAR_LOADAVG} (${VAR_TEMP})"
+echo "${RED}  : .~.'~'.~. :    ${YEL}Memory.............:${WHI} ${VAR_MEMORY}" 
+echo "${RED} ~ (   ) (   ) ~   ${YEL}Home Space.........:${WHI} ${VAR_SPACE}" 
+echo "${RED}( : '~'.~.'~' : )  ${YEL}Airsonos...........:${WHI} ${LABEL_SERVICE_1}"
+echo "${RED} ~ .~ (   ) ~. ~   ${YEL}...................:${WHI}"
+echo "${RED}  (  : '~' :  )    ${YEL}...................:${WHI}"
+echo "${RED}   '~ .~~~. ~'     ${YEL}Running Processes..:${WHI} ${VAR_PROCESSES}"
+echo "${RED}       '~'         ${YEL}IP Addresses.......:${WHI} ${VAR_IP_INTERN}(${VAR_IP_EXTERN})"
+echo ""
